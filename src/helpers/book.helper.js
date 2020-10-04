803b8474d720df3fc9cd4aeaeebeed7ade8b487e
nelson <nelson.c@geopagos.com>
2020-10-04 19:49:27 -0300

    Added bookHelper | Delete bookApiFactory

---
+++ src/helpers/book.helper.js
@@ -0,0 +1,46 @@
+
+const getQueryParams = (body) => {
+
+    let params = '';
+    let selector = '';
+
+    for (const key in body) {
+
+      switch (key) {
+
+        case 'title':
+          selector = 'intitle:';
+          break;
+
+        case 'authors':
+          selector = 'inauthor:';
+          break;
+
+        case 'isbn_10' || 'isbn_13':
+          selector = 'isbn:';
+          break;
+
+        case 'categories':
+          selector = 'subject:';
+          break;
+
+        case 'publisher':
+          selector = 'inpublisher:';
+          break;
+
+        default:
+          selector = '';
+          break;
+      }
+
+      if(!selector) continue;
+
+      params = params + '+' + selector + body[key];
+
+    }
+    return params;
+  }
+
+module.exports = {
+    getQueryParams
+};
--- src/modules/book/api/googleBook.js
+++ src/modules/book/api/googleBook.js
@@ -1,11 +1,12 @@
 const axios = require("../../../config/axios");
+const { getQueryParams } = require('../../../helpers/book.helper');
 
 class googleBook{
 
     findBooks = async (body) =>
     {
 
-        let queryParams = this._getQueryParams(body);
+        let queryParams = getQueryParams(body);
 
         let response = await axios.get(`volumes?q=${queryParams}&startIndex=${body.offset}`);
 
@@ -16,48 +17,6 @@ class googleBook{
         return response.data;
 
     }
-
-    _getQueryParams(body) {
-
-        let params = '';
-        let selector = '';
-    
-        for (const key in body) {
-    
-          switch (key) {
-    
-            case 'title':
-              selector = 'intitle:';
-              break;
-    
-            case 'authors':
-              selector = 'inauthor:';
-              break;
-    
-            case 'isbn_10' || 'isbn_13':
-              selector = 'isbn:';
-              break;
-    
-            case 'categories':
-              selector = 'subject:';
-              break;
-    
-            case 'publisher':
-              selector = 'inpublisher:';
-              break;
-    
-            default:
-              selector = '';
-              break;
-          }
-    
-          if(!selector) continue;
-    
-          params = params + '+' + selector + body[key];
-    
-        }
-        return params;
-      }
 }
 
 module.exports = googleBook;
